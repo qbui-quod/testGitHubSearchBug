3b2117780351619a31ac2be704e9ac7a2b8b8074
Quan Bui <quan@quod.ai>
2020-12-30 17:28:13 +0700

    Create test.cpp
    Best program ever.

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World! I just wrote this program directly on GitHub without testing the code at all! :)" << endl;
+
+  return 0;
+}
